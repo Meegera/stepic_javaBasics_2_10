170a93077ba5a3a12152068feb1c02f16013fbe7
Гера <gretadonskova@gmail.com>
2023-02-18 20:58:17 +0300

    2_10_4 commit

---
+++ src/Calculate.java
@@ -0,0 +1,60 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class Calculate {
+    static public void main (String []args) throws FileNotFoundException {
+        File file = new File("example.txt");
+        Scanner sc = new Scanner (file);
+        String line = sc.nextLine();
+        double a = 0.0;
+        double b = 0.0;
+        String znak = " ";
+        int m=0;
+        boolean flag = true;
+        for(String num:line.split(" ")) {
+            m++;
+            if(m==1 | m==3) {
+                try {
+                    if(m == 1) a = Double.valueOf(num);
+                    if(m == 3) b = Double.valueOf(num);
+                }catch(NumberFormatException e) {
+                    System.out.print("Error! Not number\n");
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if(flag) {
+            m=0;
+            for(String num:line.split(" ")) {
+                m++;
+                if(m==2) {
+                    try {
+                        znak = num;
+                        if(!(num.equals("/") | num.equals("+") | num.equals("-") | num.equals("*"))) throw new Exception("Operation Error!");
+                    }catch(Exception s){
+                        System.out.print(s.getMessage());
+                        flag=false;
+                        break;
+                    }
+                }
+            }
+        }
+        if(flag) {
+            switch(znak){
+                case("+"): System.out.print(a + b);
+                    break;
+                case("-"): System.out.print(a - b);
+                    break;
+                case("/"):
+                    if(b==0.0) System.out.print("Error! Division by zero");
+                    else     System.out.print(a / b);
+                    break;
+                case("*"): System.out.print(a * b);
+                    break;
+            }
+        }
+
+    }
+}
